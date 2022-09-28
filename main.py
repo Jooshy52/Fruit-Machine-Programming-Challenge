46289e1594966253eac58efd7685ba00369dec02
Jooshy <114580969+Jooshy52@users.noreply.github.com>
2022-09-28 11:29:02 +0000

    Set up for Threading by putting most of View in a function

--- main.py
+++ main.py
@@ -45,87 +45,128 @@ class View(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
 
-        self.button_names = ["Nudge-1", "Nudge-2", "Nudge-3"]
-        self.display_labels = []
-        self.fruit_labels = []
-        self.nudge_buttons = []
-
-        # field options
-        options = {'padx': 5, 'pady': 5}
-
-        # add padding to the frame and show it
-        self.grid(padx=2, pady=2, sticky=tk.NSEW)
-
-        # result label
-        self.result_label = ttk.Label(self, text=f"£{1.00}")
-        self.result_label.grid(column=2, row=4, sticky=tk.E, padx=10, pady=5)
-
-        # win label
-        self.win_label = ttk.Label(self,
-                                   text=f"Your results will be shown here")
-        self.win_label.grid(column=1,
-                            row=4,
-                            rowspan=2,
-                            sticky=tk.NS,
-                            padx=10,
-                            pady=5)
-
-        # fruit_displays
-        for index in range(3):
-            # image of fruit
-            self.display_label = tk.Label(self,
-                                          text="temporary",
-                                          font="Monospace 10",
-                                          bg="black",
-                                          fg='white')
-            self.display_label.grid(column=index, row=1, **options)
-
-            self.display_labels.append(self.display_label)
-
-            # name of fruit
-            self.fruit_label = ttk.Label(self,
-                                         text="temporary",
-                                         font=("Monospace 10", 8))
-            self.fruit_label.grid(column=index, row=2)
-
-            self.fruit_labels.append(self.fruit_label)
-
-        # nudge buttons
-        for index in range(3):
-            n = self.button_names[index]
-
-            self.nudge_button = ttk.Button(
+        def start_game():
+            self.button_names = ["Nudge-1", "Nudge-2", "Nudge-3"]
+            self.display_labels = []
+            self.fruit_labels = []
+            self.nudge_buttons = []
+    
+            # field options
+            options = {'padx': 5, 'pady': 5}
+    
+            # add padding to the frame and show it
+            self.grid(padx=2, pady=2, sticky=tk.NSEW)
+    
+            # result label
+            self.result_label = ttk.Label(self, text=f"£{1.00}")
+            self.result_label.grid(column=2, row=4, sticky=tk.E, padx=10, pady=5)
+    
+            # win label
+            self.win_label = ttk.Label(self,
+                                       text=f"Your results will be shown here")
+            self.win_label.grid(column=1,
+                                row=4,
+                                rowspan=2,
+                                sticky=tk.NS,
+                                padx=10,
+                                pady=5)
+    
+            # fruit_displays
+            for index in range(3):
+                # image of fruit
+                self.display_label = tk.Label(self,
+                                              text="temporary",
+                                              font="Monospace 10",
+                                              bg="black",
+                                              fg='white')
+                self.display_label.grid(column=index, row=1, **options)
+    
+                self.display_labels.append(self.display_label)
+    
+                # name of fruit
+                self.fruit_label = ttk.Label(self,
+                                             text="temporary",
+                                             font=("Monospace 10", 8))
+                self.fruit_label.grid(column=index, row=2)
+    
+                self.fruit_labels.append(self.fruit_label)
+    
+            # nudge buttons
+            for index in range(3):
+                n = self.button_names[index]
+    
+                self.nudge_button = ttk.Button(
+                    self,
+                    text=n,
+                    command=lambda index=index + 1: self.nudge_Callback(index),
+                    style="C.TButton")
+                self.nudge_button.grid(column=index, row=3, **options)
+    
+                self.nudge_buttons.append(self.nudge_button)
+    
+            # roll
+            self.roll_button = ttk.Button(
                 self,
-                text=n,
-                command=lambda index=index + 1: self.nudge_Callback(index),
+                text="Roll",
+                command=lambda t="roll_button": self.roll_Callback(),
                 style="C.TButton")
-            self.nudge_button.grid(column=index, row=3, **options)
+            self.roll_button.grid(column=0,
+                                  row=4,
+                                  rowspan=2,
+                                  sticky=tk.W,
+                                  padx=20,
+                                  pady=5)
+    
+            # exit button
+            self.exit_button = ttk.Button(
+                self,
+                text="Win",
+                command=lambda t="exit_button": self.exit_callback(),
+                style="C.TButton")
+            self.exit_button.grid(column=2, row=5, sticky=tk.E, **options)
+    
+            self.disable_buttons()
+            self.controller = None
+    
 
-            self.nudge_buttons.append(self.nudge_button)
-
-        # roll
-        self.roll_button = ttk.Button(
-            self,
-            text="Roll",
-            command=lambda t="roll_button": self.roll_Callback(),
-            style="C.TButton")
-        self.roll_button.grid(column=0,
-                              row=4,
-                              rowspan=2,
-                              sticky=tk.W,
-                              padx=20,
-                              pady=5)
-
-        # exit button
-        self.exit_button = ttk.Button(
-            self,
-            text="Win",
-            command=lambda t="exit_button": self.exit_callback(),
-            style="C.TButton")
-        self.exit_button.grid(column=2, row=5, sticky=tk.E, **options)
-
-        self.disable_buttons()
-        self.controller = None
+        def disable_buttons(self):
+            for index in range(3):
+                self.nudge_buttons[index].config(state="disabled")
+    
+            self.exit_button.config(state="disabled")
+    
+        def enable_buttons(self):
+            for index in range(3):
+                self.nudge_buttons[index].config(state="enabled")
+    
+            self.exit_button.config(state="enabled")
+    
+        def exit_callback(self):
+            """
+          Handle button click event
+          :return:
+          """
+            if self.controller:
+                self.controller.exit()
+    
+        def nudge_Callback(self, index):
+            """
+          Handle button click event
+          :return:
+          """
+            if self.controller:
+                self.disable_buttons()
+                self.controller.nudge(index)
+    
+        def roll_Callback(self):
+            """
+          Handle button click event
+          :return:
+          """
+            if self.controller:
+                self.enable_buttons()
+                self.controller.roll()
+    
 
     def set_controller(self, controller):
         """
@@ -134,45 +175,6 @@ class View(ttk.Frame):
       :return:
       """
         self.controller = controller
-
-    def disable_buttons(self):
-        for index in range(3):
-            self.nudge_buttons[index].config(state="disabled")
-
-        self.exit_button.config(state="disabled")
-
-    def enable_buttons(self):
-        for index in range(3):
-            self.nudge_buttons[index].config(state="enabled")
-
-        self.exit_button.config(state="enabled")
-
-    def exit_callback(self):
-        """
-      Handle button click event
-      :return:
-      """
-        if self.controller:
-            self.controller.exit()
-
-    def nudge_Callback(self, index):
-        """
-      Handle button click event
-      :return:
-      """
-        if self.controller:
-            self.disable_buttons()
-            self.controller.nudge(index)
-
-    def roll_Callback(self):
-        """
-      Handle button click event
-      :return:
-      """
-        if self.controller:
-            self.enable_buttons()
-            self.controller.roll()
-
 
 class Controller:
     def __init__(self, model, view):
